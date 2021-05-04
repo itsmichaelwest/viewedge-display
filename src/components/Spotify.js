6e05cda6e948496ed166ebc219aed225f32d6363
Michael West <19785650+itsmichaelwest@users.noreply.github.com>
2021-05-04 18:33:27 +0100

    Stop app crashing when spotify is closed

--- src/components/Spotify.js
+++ src/components/Spotify.js
@@ -11,6 +11,8 @@ export default function Spotify() {
             setSpotify(ipcRenderer.sendSync('spotify-np'))
         }, 1000)
 
+        console.log(spotify)
+
         if (spotify && spotify !== 'no') {
             document.documentElement.style.setProperty('--pri-color', 'white')
             document.documentElement.style.setProperty('--pri-opacity', '0.6')
@@ -29,43 +31,43 @@ export default function Spotify() {
     return (
         <>
         { spotify !== 'no' ? 
-            <div className="spotify">
-                { spotify ?
-                    <div className="spotify-info">
-                        <img className="artwork" src={spotify.item.album.images[0].url} />
-                        <div className="spotify-details">
-                            <div className="song">
-                                {spotify.item.name}
-                            </div>
-                            <div className="artist">
-                                {spotify.item.artists.map((artist) => {
-                                    return (
-                                        <span>
-                                            {artist.name}
-                                        </span>
-                                    )
-                                })}
-                            </div>
-                            <div className="album">
-                                {spotify.item.album.name}
-                            </div>
-                            <div className="progress">
-                                <div className="progress-elapsed" style={{ width: `${(spotify.progress_ms / spotify.item.duration_ms) * 100}%` }} />
-                                <div className="progress-total" />
-                            </div>
+        <></>
+        :
+        <div className="spotify">
+            { spotify ?
+                <div className="spotify-info">
+                    <img className="artwork" src={spotify.item.album.images[0].url} />
+                    <div className="spotify-details">
+                        <div className="song">
+                            {spotify.item.name}
                         </div>
-                        <div className="album-artwork-blur">
-                            <img src={spotify.item.album.images[0].url} />
+                        <div className="artist">
+                            {spotify.item.artists.map((artist) => {
+                                return (
+                                    <span>
+                                        {artist.name}
+                                    </span>
+                                )
+                            })}
+                        </div>
+                        <div className="album">
+                            {spotify.item.album.name}
+                        </div>
+                        <div className="progress">
+                            <div className="progress-elapsed" style={{ width: `${(spotify.progress_ms / spotify.item.duration_ms) * 100}%` }} />
+                            <div className="progress-total" />
                         </div>
                     </div>
-                    :
-                    <button className="spotify-login" onClick={spotifyLogin}>
-                        Click to authorize Spotify
-                    </button>
-                }
-            </div>
-        :
-        <></>
+                    <div className="album-artwork-blur">
+                        <img src={spotify.item.album.images[0].url} />
+                    </div>
+                </div>
+                :
+                <button className="spotify-login" onClick={spotifyLogin}>
+                    Click to authorize Spotify
+                </button>
+            }
+        </div>
         }
         </>
     )
